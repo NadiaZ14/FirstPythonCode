58fb4e0c692396df2582b2be5941c74d4a1bdf60
Nadia <nadiazia94@outlook.com>
2022-04-27 17:40:28 +0100

    new file:   .idea/.gitignore 	new file:   .idea/MyFirstPythonProject.iml 	new file:   .idea/inspectionProfiles/profiles_settings.xml 	new file:   .idea/misc.xml 	new file:   .idea/modules.xml 	new file:   myfirstscript.py

---
+++ myfirstscript.py
@@ -0,0 +1,25 @@
+print("hello")
+
+# #create variable called num1 and assign it a value of 5
+# num1 = 5
+# print(num1)
+# print(type(num1))
+#
+# num1 = "Nadia"
+# print(num1)
+# print(type(num1))
+# #
+# firstName = input("Type in your first name: ")
+# lastname = input("Type in your last name: ")
+#
+# print(firstName + " " + lastname)
+#
+# num1 = "5"
+# num2 = "6"
+# print(num1+num2)
+
+strl1= input("Type in first number: ")
+strl2 = input("Type in  second number: ")
+num1 = int(strl1)
+num2 = int(strl2)
+print(num1+num2)
